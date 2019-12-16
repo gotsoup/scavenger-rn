b2531f899dcf4915a0f22eb8e93758e2103e864c
Michelle Morry <michelle@smble.io>
2019-12-16 15:30:55 -0800

    removing slide animation from nav buttons

--- App.tsx
+++ App.tsx
@@ -1,42 +1,49 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Animated, Easing } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import configureStore from './src/utils/store';
 import Camera from './src/screens/Camera';
 import Checklist from './src/screens/Checklist';
 import Gallery from './src/screens/Gallery';
 import ItemModal from './src/components/ItemModal';
 
-
 const MainNavigator = createStackNavigator(
   {
-    Checklist: { screen: Checklist },
-    Gallery: { screen: Gallery },
-    Camera: { screen: Camera }
+    Checklist: {screen: Checklist},
+    Gallery: {screen: Gallery},
+    Camera: {screen: Camera},
   },
   {
     defaultNavigationOptions: {
-      header: null
-    }
-  }
+      header: null,
+    },
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 0,
+        timing: Animated.timing,
+        easing: Easing.step0,
+      },
+    }),
+  },
 );
 
 const RootStack = createStackNavigator(
   {
     Main: {
-      screen: MainNavigator
+      screen: MainNavigator,
     },
     ItemModal: {
-      screen: ItemModal
-    }
+      screen: ItemModal,
+    },
   },
   {
     mode: 'modal',
-    headerMode: 'none'
-  }
-)
+    headerMode: 'none',
+  },
+);
 
 const store = configureStore();
 
